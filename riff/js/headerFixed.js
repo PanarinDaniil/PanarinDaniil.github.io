7ab2b168e8f2cede206755c8178e4ea8dc6bc930
Dan Panarin <panarin.daniil@gmail.com>
2017-01-23 13:03:47 +0200

    header anchor + ашчуы

--- riff/js/headerFixed.js
+++ riff/js/headerFixed.js
@@ -39,13 +39,11 @@ $(document).ready(function(){
 	    offSetManager();
 	}
 	offSetManager();
-
 	$(".mob-btn").on('click', function(e){
         e.preventDefault();
         $(this).toggleClass("active");
         $(".nav-layout, .header-nav").toggle($(this).hasClass("active"));
     });
-
     $('.nav-layout').click(function() {
         $(".mob-btn").removeClass("active");
         $(".nav-layout, .header-nav").hide('fast');
@@ -55,18 +53,4 @@ $(document).ready(function(){
 		changeHash: true,
 		scrollSpeed: 750
 	});
-
-    // $('a[href*=#]:not([href=#])').click(function () {
-    //     if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-
-    //         var target = $(this.hash);
-    //         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    //         if (target.length) {
-    //             $('html,body').animate({
-    //                 scrollTop: target.offset().top
-    //             }, 2000);
-    //             return false;
-    //         }
-    //     }
-    // });
 });
